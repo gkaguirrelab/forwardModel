09ad71e6aa5539d915f59eaf622d689523623ff7
gkaguirre <gkaguirre@me.com>
2022-03-30 17:33:47 -0400

    Bring back the glm model

---
+++ code/models/@glm/genhrf.m
@@ -0,0 +1,42 @@
+function genhrf(obj)
+% Creates and stores an HRF using the FLOBS eigenvectors
+%
+% Syntax:
+%   obj.genhrf
+%
+% Description:
+%   Creates an HRF kernel for convolution. The stored kernel has unit area
+%   so that it preserves signal area after convolution. The kernel is
+%   specified in a time x 1 vector orientation.
+%
+%   Typical values for the FLOBS weights are [0.86 0.09 0.01];
+%
+% Inputs:
+%   none
+%
+% Optional key/value pairs:
+%   none
+%
+% Outputs:
+%   none
+%
+
+
+% Obj variables
+stimDeltaT = obj.dataDeltaT;
+hrfParams = obj.hrfParams;
+
+% Obtain the FLOBS vectors
+flobsbasis = returnFlobsVectors(stimDeltaT);
+
+% Creat the HRF
+hrf = flobsbasis*hrfParams';
+
+% Normalize the kernel to have unit area
+hrf = hrf/sum(abs(hrf));
+
+% Store the hrf in the object.
+obj.hrf = hrf;
+
+end
+
---
+++ code/models/@glm/initial.m
@@ -0,0 +1,30 @@
+function x0 = initial(obj)
+% Returns initial guess for the model parameters
+%
+% Syntax:
+%   x0 = obj.initial()
+%
+% Description:
+%
+% Inputs:
+%   none
+%
+% Optional key/value pairs:
+%   none
+%
+% Outputs:
+%   x0                    - 1xnParams vector.
+%
+
+
+% Obj variables
+nParams = obj.nParams;
+
+% Assign the x0 variable
+x0 = zeros(1,nParams);
+
+% Assemble X0
+x0(1:nParams) = 0;
+
+end
+
